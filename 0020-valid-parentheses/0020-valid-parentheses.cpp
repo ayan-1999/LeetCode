0b7af050ade26400c2bed012f56d06247a7da108
Ayan Manna <143441885+ayan-1999@users.noreply.github.com>
2024-09-06 02:23:35 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (56.40%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> stk;
+        for(char c :s)
+        {
+            if(c=='(' || c=='{' || c=='[')
+                stk.push(c);
+            else
+            {
+                if(!stk.empty())
+                {
+                    if(c==')' && stk.top()=='('|| c=='}'  && stk.top()=='{'|| c==']' && stk.top()=='[')
+                        stk.pop();
+                    else return false;
+                }
+                else return false;
+            }
+        }
+        return stk.empty()?true:false;
+        
+    }
+};
