013a95c9b04a09c9c3064d1ba22c010891f0603e
Ayan Manna <143441885+ayan-1999@users.noreply.github.com>
2024-09-10 03:24:27 +0530

    Time: 3 ms (44.83%), Space: 7.7 MB (92.41%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -6,24 +6,17 @@ public:
         int i=0;
         int j=0;
         string ans;
-            while(i<m && j<n){
-                if(ans.size()%2==0){
+            while(i<m || j<n){
+                if(i<m){
                     ans.push_back(word1[i]);
                     i++;
                 }
-                else{
+                if(j<n){
                      ans.push_back(word2[j]);
                     j++;
                 }
             }
-        while(i<m){
-            ans.push_back(word1[i]);
-            i++;
-        }
-         while(j<n){
-            ans.push_back(word2[j]);
-            j++;
-        }
+       
       return ans;                 
     }
 };
