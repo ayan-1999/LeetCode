b9d61a98fe77534195fc930358fe3feb6ef7568a
Ayan Manna <143441885+ayan-1999@users.noreply.github.com>
2024-09-10 03:38:40 +0530

    Time: 4 ms (71.10%), Space: 15.9 MB (77.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,7 +5,8 @@ public:
         for(int i=0;i<prices.size();i++)
         {
           int buy=prices[i];
-            while((i+1)<prices.size() && (prices[i]<=prices[i+1]))i++;
+            while((i+1)<prices.size() 
+                  && (prices[i] <=prices[i+1]))i++;                
             profit+=prices[i]-buy;
         }
         return profit;
