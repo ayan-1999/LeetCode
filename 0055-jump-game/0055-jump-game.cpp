bb0e8e430d96ae5e2230b1a3cdbbdf79c55e6189
Ayan Manna <143441885+ayan-1999@users.noreply.github.com>
2024-09-11 04:45:54 +0530

    Time: 40 ms (75.73%), Space: 51.1 MB (81.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int goal = nums.size() - 1;  // Initialize goal to the last index
+        int goal = nums.size() - 1;  
+        // Initialize goal to the last index
         
-        // Special case: if there is only one element, you're already at the goal
+        // Special case: if there is only one element, you're      already at the goal
         if (nums.size() == 1)
             return true;
         
-        // Iterate from the second last element to the first
+ // Iterate from the second last element to the first
         for (int i = nums.size() - 2; i >= 0; i--) {
-            // If the current position can jump to or beyond the goal
+// If the current position can jump to or beyond the goal
             if (i + nums[i] >= goal)
                 goal = i;  // Update the goal to the current index
         }
